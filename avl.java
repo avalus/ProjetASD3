23f59d38319496bc9e0a84ad1e7a6b41b601b34a
Valery MOROZOV <E14D399F@S042V2pc19.ensinfo.sciences.univ-nantes.prive>
2014-11-04 14:52:13 +0100

    wip

--- avl.java
+++ avl.java
@@ -3,110 +3,98 @@ import java.util*;
 
 class Avl
 {
-	private int elt;
-	private int bal;
-	private Avl d;
-	private Avl g;
-	public Avl<k>( int id,int bal)
+	private int elt, bal;
+	private Avl g, d;
+
+	public Avl(int id, int bal)
 	{
-	  elt = id;
-	  this.bal = bal;
-	  d = null;
-	  g = null;
+		elt = id;
+		this.bal = bal;
+		d = null;
+		g = null;
 	}
 	
-	public (avl,int) ajouter( int x, avl A )
+	/* Retourne l'arbre après ajout de l'élément x + équilibrage*/
+	public Avl ajouter(int x, Avl A)
 	{	    
-/* retourne le nouvel arbre et la variation de la hauteur */
- 
 		if (A = null)
 		{
  	 		A = new Avl(0,0); 
-			A.elt ← x; A.bal ← 0;
-			return (A,1);
+			A.elt = x; A.bal = 0;
+
+			return (A);
 		}
 		else if(x = A.elt)
-			return (A,0);
-		else if (x > A.elt) alors
-			(A.d,int h)= ajouter(x,A.d);
+		{
+			return (A);
+		}
+		else if (x > A.elt)
+		{
+			A.d = ajouter(x, A.d);
+		}
+		else
+		{	
+			A.g = ajouter(x, A.g); 
+			int h = A.g.bal; h = -h;
+		}
+
+		if (h = 0)
+		{
+			return (A);
+		}
 		else
 		{
-			(A.g,int h) = ajouter(x,A.g); h ← -h;}
-			if (h = 0) 
-				return (A,0);
-			else
-				A.bal = A.bal + h;
-				A =  equilibrer(A);
-			if (A.bal = 0) alors
-				return (A,0);
-			else
-  			return (A,1);
+			A.bal = A.bal + h;
+			A = equilibrer(A);
+		}
+		
+		if (A.bal = 0)
+		{
+			return (A);
+		}
+		else
+		{
+  			return (A);
 		}
 	}
 
-	public Avl rotG(Avl A)
-    {
-		Avl B; int a,b;
-		B = A.d;
-		a = A.bal; b = B.bal;
-		A.d = B.g; B.g = A; /* rotation */
-		A.bal = a-max(b,0)-1;
-		B.bal = min(a-2,a+b-2,b-1);
-		return B;
-	}
-     
-    public Avl rotD(Avl A)
-    {
-		Avl B; int a,b;
-		B = A.g;
-		a = A.bal; b = B.bal;
-		A.g = B.d; B.d = A; /* rotation */
-		A.bal = a-max(b,0)-1;
-		B.bal = min(a-2,a+b-2,b-1);
-		return B;
-	}
-
-    public Avl dRotG(Avl A)
-	{
-		A.d = rotD(A.d);
-		return rotG(A);
-	}
-
-     public Avl dRotD(Avl A)
-	{
-		A.g = rotG(A.g);
-		return rotD(A);
-	}
-   
-
-/*****Entrée A arbre tel que A g , A d sont AVL -2 ≤ bal(A) ≤ 2*****/
-    public avl equilibrer(avl A) 
+	/* Entrée A arbre tel que A.g , A.d sont des AVL -2 ≤ bal(A) ≤ 2 */
+	public Avl equilibrer(Avl A) 
 	{   
 		if (A.bal = 2) 
 		{
-			if (A.d.bal >= 0) 
+			if (A.d.bal >= 0)
+			{
 				return rotG(A);
+			}
 			else 
 			{ 
 				A.d = rotD(A.d);
+
 				return rotG(A);
 			}
 		}
 		else if (A.bal = -2) 
 		{
 			if (A.g.bal <= 0)
+			{
 				return rotD(A);
+			}
 			else 
 			{
 				A.g = rotG(A.g);
+
 				return rotD(A);
 			}
 		}
-		else return A;
-	}    
+		else 
+		{
+			return A;
+		}
+	}
 
-	public (Avl,int) enlever(int x, Avl A)
-/* retourne le nouvel arbre et la variation de la hauteur*/\
+	/* Retourne l'arbre après suppression de l'élément x + équilibrage */
+	public Avl enlever(int x, Avl A)
 	{
 		if (A = null)
 		{
@@ -114,67 +102,140 @@ class Avl
 		}
 		else if (x > A.elt) 
 		{
-			(A .d ,h) = enlever(x,A.d);
+			A.d = enlever(x, A.d);
 		}
 		else if (x < A.elt) 
 		{
-    		(A .g ,int h) = enlever(x,A.g); h ← -h;}
+	    		A.g = enlever(x, A.g);
+		        int h = A.g.bal; h = -h;
+		}
 		else if (A.g = null) 
 		{
-			return (A.d,-1);
+			return A.d;
 		}
-		else if (A.d = null
+		else if (A.d = null)
 		{
-			return (A.g,-1);
+			return A.g;
 		}
 		else 
 		{
 			A.elt = min(A.d);
-			(A.d,h) = OTERMIN(A.d);
+			A.d = oterMin(A.d);
 		}
-		if (h == 0) return (A,0);
+		if (h == 0) 
+		{
+			return A;
+		}
 		else 
 		{
-			A.bal ← A.bal + h;
-			A ← equilibrer(A);
-			if (A.bal = 0) alors return (A,-1);
-			else return (A,0);
+			A.bal = A.bal + h;
+			A = equilibrer(A);
+			if (A.bal = 0)
+			{
+				return A;
+			}
+			else
+			{
+				return A;
+			}
 		}
 	}
+	
+	/* Retourne l'arbre après ROTATION GAUCHE */
+	public Avl rotG(Avl A)
+    	{
+		Avl B; 
+		int a, b;
 
-	public (Avl,int) oterMin(Avl A) 
+		B = A.d;
+		a = A.bal; b = B.bal;
+		A.d = B.g; B.g = A; /* rotation */
+		A.bal = a-max(b,0)-1;
+		B.bal = min(a-2, a+b-2, b-1);
+
+		return B;
+	}
+
+	/* Retourne l'arbre après ROTATION DROITE */
+	public Avl rotD(Avl A)
+	{
+		Avl B; 
+		int a,b;
+
+		B = A.g;
+		a = A.bal; b = B.bal;
+		A.g = B.d; B.d = A; /* rotation */
+		A.bal = a-max(b,0)-1;
+		B.bal = min(a-2, a+b-2, b-1);
+
+		return B;
+	}
+
+	/* Retourne l'arbre après DOUBLE ROTATION GAUCHE */
+    	public Avl dRotG(Avl A)
+	{
+		A.d = rotD(A.d);
+
+		return rotG(A);
+	}
+
+	/* Retourne l'arbre après DOUBLE ROTATION DROITE */
+     	public Avl dRotD(Avl A)
+	{
+		A.g = rotG(A.g);
+
+		return rotD(A);
+	}
+	
+	/* Retourne l'arbre après suppression de l'élément min + équilibrage */
+	public Avl oterMin(Avl A) 
 	{
 		if(A.g == null) 
 		{
 			min = A.elt;
-			return (A.d,-1);
+
+			return A.d;
 		}
 		else
 		{
-			(A .g ,int h) ← oterMin(A.g); h = -h;
+			A.g = oterMin(A.g); 
+			int h = A.g.bal; h = -h;
 		}
 		if (h == 0)
-			return(A,0);
-		else {
-			A.bal ← A.bal + h;
-			A ← equilibrer(A);
-			if (A.bal = 0) 
-				return (A,-1);
+		{
+			return A;
+		}
+		else 
+		{
+			A.bal = A.bal + h;
+			A = equilibrer(A);
+			if (A.bal = 0)
+			{
+				return A;
+			}
 			else
-				return (A,0);
+			{
+				return A;
 			}
 		}
-
+	
+	/* Retourne l'élement minimum de l'AVL */
 	public int min(Avl A)
 	{
 		if(A != null)
 		{
 			Avl courant = A;
-			while(courant .g != null)
+
+			while(courant.g != null)
 			{
 				courant = courant.g;
 			}
-         return courant.elt;
-		else System.out.println("ERROR, structure vide");
+
+         		return courant.elt;
+		}
+		else 
+		{
+			System.out.println("Erreur : structure vide");
+		}
 	}
 }
