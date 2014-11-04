4cf56cf31743e612fbe7481cff21d77d41c644bc
Gaston DIEDHIOU <E138905J@S042V2pc20.ensinfo.sciences.univ-nantes.prive>
2014-11-04 14:00:27 +0100

    wip

---
+++ arbre.java
@@ -0,0 +1,21 @@
+import java.lang*;
+import java.util*;
+
+class Arbre
+{
+	private char elt;
+	private Arbre []fils;
+
+	public Arbre( char id)
+	{
+	  elt = id;
+	  fils = new Arbre()[26];
+
+	}
+
+	public void inserer( char car )
+	{
+	  this.elt = car;
+	}
+
+}
