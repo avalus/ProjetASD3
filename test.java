eea3999c21e32c7bd4208e9d05ac1a84a0e89704
marcusgas <marcus-g@outlook.fr>
2014-12-03 21:46:41 +0100

    Update test.java

--- test.java
+++ test.java
@@ -7,28 +7,24 @@ public class Test
 
 	public static void main(string []args)
 	{
-		String chaine = "";
-		String fichier ="";
-		Chapitre c;
-		
-   		int [] tab;
-		tab = new int[args.length-1];
-		
-		for(int i = 1; i < args.length; ++i)
-		{	try{
-				InputStream ips = new FileInputStream(args[i]); 
-				InputStreamReader ipsr = new InputStreamReader(ips);
-				BufferedReader br=new BufferedReader(ipsr);
-				String ligne;
-				while ((ligne=br.readLine())!=null){
-					System.out.println(ligne);
-					chaine+=ligne+"\n";
-			    	}
-			        br.close(); 
-			   }		
-			  catch (Exception e){
-				System.out.println(e.toString());
-			  }
+		  try{
+
+			InputStream ips = new FileInputStream("txt"); 
+
+			InputStreamReader ipsr = new InputStreamReader(ips);
+
+			BufferedReader br=new BufferedReader(ipsr);
+
+                	Arbre a = creer(br);
+
+                        affiche(a);
+
+		}		
+
+		catch (Exception e){
+
+			System.out.println(e.toString());
+
 		}
 		
 
